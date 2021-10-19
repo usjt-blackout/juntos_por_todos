8e61ca54f8d5d0162833f87542316cfe50107bb6
rco <raphaelaoliveira79@gmail.com>
2021-10-19 00:18:46 -0300

    Commit inicial Android Studio

---
+++ app/src/main/java/br/com/usjt/juntos_por_todos/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.usjt.juntos_por_todos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
